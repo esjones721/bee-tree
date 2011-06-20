8fcbf6e7a73a9129e876c72ad052333e346a173c
Roger Kapsi <rkapsi@gmail.com>
2011-06-20 13:31:32 -0400

    More

---
+++ src/main/java/org/ardverk/btree2/DefaultNodeProvider.java
@@ -0,0 +1,95 @@
+package org.ardverk.btree2;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.ardverk.btree2.Node.Id;
+import org.ardverk.btree2.Node.Split;
+
+public class DefaultNodeProvider<K, V> implements NodeProvider<K, V> {
+
+    public final Map<Id, Node<K, V>> nodes 
+        = new LinkedHashMap<Id, Node<K, V>>();
+    
+    private final Comparator<? super K> comparator;
+    
+    public volatile Node<K, V> root;
+    
+    public DefaultNodeProvider() {
+        this(DefaultComparator.create());
+    }
+    
+    public DefaultNodeProvider(Comparator<? super K> comparator) {
+        this.comparator = comparator;
+        root = create(true);
+    }
+
+    @Override
+    public Node<K, V> get(Id pageId, Intent intent) {
+        Node<K, V> node = nodes.get(pageId);
+        return node;
+    }
+    
+    
+    @Override
+    public Node<K, V> create(boolean leaf) {
+        return add(new Node<K, V>(leaf));
+    }
+
+    public Node<K, V> add(Node<K, V> node) {
+        nodes.put(node.getId(), node);
+        return node;
+    }
+
+    @Override
+    public Comparator<? super K> comparator() {
+        return comparator;
+    }
+    
+    public void put(K key, V value) {
+        if (root.isFull()) {
+            Node<K, V> tmp = create(false);
+            Split<K, V> split = root.split(this);
+            
+            
+            
+            root = tmp;
+        }
+        
+        root.put(this, key, value);
+    }
+    
+    public static void main(String[] args) {
+        DefaultNodeProvider<String, String> t 
+            = new DefaultNodeProvider<String, String>();
+        
+        t.put("A", "A");
+        t.put("C", "C");
+        t.put("G", "G");
+        t.put("N", "N");
+        
+        System.out.println(t.root);
+        
+        t.put("H", "H");
+        
+        System.out.println(t.root);
+    }
+    
+    private static class DefaultComparator<K> implements Comparator<K> {
+        
+        private static final DefaultComparator<?> COMPARATOR 
+            = new DefaultComparator<Object>();
+        
+        @SuppressWarnings("unchecked")
+        public static <K> Comparator<K> create() {
+            return (Comparator<K>)COMPARATOR;
+        }
+        
+        @SuppressWarnings("unchecked")
+        @Override
+        public int compare(K o1, K o2) {
+            return ((Comparable<K>)o1).compareTo(o2);
+        }
+    }
+}
--- src/main/java/org/ardverk/btree2/Node.java
+++ src/main/java/org/ardverk/btree2/Node.java
@@ -3,16 +3,17 @@ package org.ardverk.btree2;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.ardverk.btree2.NodeProvider.Intent;
 
 public class Node<K, V> {
 
-    private static final int t = 4;
+    private static final int t = 2;
     
-    private final List<Entry<K, V>> entries = new ArrayList<Entry<K, V>>();
+    private final List<Entry<K, V>> entries = new ArrayList<Entry<K, V>>(2*t-1);
     
-    private final List<Id> nodes = new ArrayList<Id>();
+    private final List<Id> nodes = new ArrayList<Id>(2*t);
     
     private final Id nodeId;
     
@@ -32,15 +33,15 @@ public class Node<K, V> {
     }
     
     public int size() {
-        return nodes.size();
+        return entries.size();
     }
     
     public boolean isEmpty() {
-        return nodes.isEmpty();
+        return entries.isEmpty();
     }
     
     public boolean isFull() {
-        return size() >= 2*t-1;
+        return size() >= 2*t;
     }
     
     public Entry<K, V> get(NodeProvider<K, V> provider, K key) {
@@ -79,14 +80,113 @@ public class Node<K, V> {
     }
     
     public Entry<K, V> put(NodeProvider<K, V> provider, K key, V value) {
-        return null;
+        Comparator<? super K> comparator = provider.comparator();
+        int index = EntryUtils.binarySearch(entries, key, comparator);
+        
+        if (leaf) {
+            
+            Entry<K, V> existing = null;
+            if (index < 0) {
+                entries.add(-index - 1, new Entry<K, V>(key, value));
+            } else {
+                existing = entries.set(index, new Entry<K, V>(key, value));
+            }
+            
+            return existing;
+        }
+        
+        if (index < 0) {
+            index = -index - 1;
+        }
+        
+        int pos = Math.min(size(), index);
+        Entry<K, V> entry = entries.get(pos);
+        
+        int diff = comparator.compare(key, entry.getKey());
+        
+        Id nodeId = null;
+        if (diff < 0) {
+            nodeId = nodes.get(index);
+        } else {
+            nodeId = nodes.get(index + 1);
+        }
+        
+        Node<K, V> node = provider.get(nodeId, Intent.WRITE);
+        
+        return node.put(provider, key, value);
     }
     
-    public Entry<K, V> split() {
-        return null;
+    public Split<K, V> split(NodeProvider<K, V> provider) {
+        Node<K, V> dst = provider.create(leaf);
+        
+        int size = size();
+        int m = size/2;
+        
+        System.out.println("BEFORE: " + this);
+        
+        Entry<K, V> median = entries.remove(m);
+        for (int i = 0; i < (size-m-1); i++) {
+            dst.entries.add(entries.remove(m));
+            
+            if (!leaf) {
+                dst.nodes.add(nodes.remove(m));
+            }
+        }
+        
+        if (!leaf) {
+            dst.nodes.add(nodes.remove(m));
+        }
+        
+        System.out.println("MEDIAN: " + median);
+        System.out.println("LEFT: " + this);
+        System.out.println("RIGHT: " + dst);
+        
+        return new Split<K, V>(median, dst.getId());
+    }
+    
+    @Override
+    public String toString() {
+        return nodeId + " @ " + entries + ", " + nodes;
+    }
+    
+    public static class Split<K, V> {
+        
+        private final Entry<K, V> median;
+        
+        private final Id nodeId;
+
+        public Split(Entry<K, V> median, Id nodeId) {
+            this.median = median;
+            this.nodeId = nodeId;
+        }
     }
     
     public static class Id {
         
+        private static final AtomicInteger COUNTER = new AtomicInteger();
+        
+        private final int value = COUNTER.incrementAndGet();
+        
+        @Override
+        public int hashCode() {
+            return value;
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            } else if (!(o instanceof Id)) {
+                return false;
+            }
+            
+            Id other = (Id)o;
+            return value == other.value;
+        }
+        
+        @Override
+        public String toString() {
+            return Integer.toString(value);
+        }
     }
 }
