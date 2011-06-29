e82720dca5df35bcbd4f605ca0db6ef1fe2e5f74
Roger Kapsi <rkapsi@gmail.com>
2011-06-29 09:37:33 -0400

    More

---
+++ src/main/java/org/ardverk/btree/RootNode.java
@@ -0,0 +1,10 @@
+package org.ardverk.btree;
+
+public class RootNode {
+
+    private final NodeProvider provider;
+    
+    public RootNode(NodeProvider provider) {
+        this.provider = provider;
+    }
+}
